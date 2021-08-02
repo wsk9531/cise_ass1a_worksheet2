398826dbbfd4d63b53aec75f174a0f2392a2e4e1
ben-wsk9531 <wsk9531@autuni.ac.nz>
2021-08-03 11:49:47 +1200

    fixed test to work!

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -11,7 +11,7 @@ import React from 'react';
     //basic equality demo
   describe('Addition', () => {
     it('knows that 2 and 2 make 4', () => {
-      expect(2 + 2).toBe(5);
+      expect(2 + 2).toBe(4);
     });
   });
 
